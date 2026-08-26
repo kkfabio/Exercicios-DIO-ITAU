decfa6012667e6c5a31698b83c47f0a376c99b6c
Fábio Nunes <fabionascfilho@gmail.com>
2026-08-26 13:00:52 -0300

    Melhorando a lógica do Exercicio 6

--- src/main/java/Exercicio6.java
+++ src/main/java/Exercicio6.java
@@ -1,35 +1,41 @@
-    import java.util.Scanner;
+import java.util.Scanner;
 
-    public class Exercicio6 {
-        public static void main(String[] args) {
+public class Exercicio6 {
+    public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("Digite um numero: ");
-            int num1 = sc.nextInt();
+        System.out.println("Digite um numero: ");
+        int num1 = sc.nextInt();
 
-            System.out.println("Digite outro numero:\n(Esse número deve ser maior que o primeiro!)");
-            int num2 = sc.nextInt();
+        int num2 = 0;
 
-            System.out.println("Você quer ver todos os números pares, ou ímpares? ");
-            String opcao = sc.next();
-
-            if (opcao.equalsIgnoreCase("pares")){
-                for (int i = num2; i >= num1; i--) {
-                    if (i % 2 == 0){
-                        System.out.println(i);
-                    }
-                }
+        do {
+            System.out.printf("Digite outro número (Esse outro número deve ser maior que %s): ", num1);
+            num2 = sc.nextInt();
+            if (num1 >= num2) {
+                System.out.printf("Erro, o segundo número deve ser maior que o %s\n", num1);
             }
+        } while (num2 <= num1);
 
-            if (opcao.equalsIgnoreCase("impares")){
-                for (int i = num2; i >= num1; i--){
-                    if (i % 2 != 0){
-                        System.out.println(i);
-                    }
-                }
+        String opcao = "";
+        do {
+            System.out.println("Digite a sua opção: \nPares\nÍmpares");
+            opcao = sc.next();
+            if (!opcao.equalsIgnoreCase("pares") && !opcao.equalsIgnoreCase("impares")) {
+                System.out.println("Erro, escolha uma das opções acima.");
+            } else {
+                continue;
             }
+        } while (!opcao.equalsIgnoreCase("pares") && !opcao.equalsIgnoreCase("impares"));
 
-
+        for (int i = num2; i >= num1; i--) {
+            if (opcao.equalsIgnoreCase("pares") && i % 2 == 0) {
+                System.out.println(i);
+            } else if (opcao.equalsIgnoreCase("impares") && i % 2 != 0) {
+                System.out.println(i);
+            }
         }
+
     }
+}
